3d5468c8fda78e010d43470b1d695bc895178878
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2023-09-22 03:58:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 function getFormvalue() {
     //Write your code here
 
+	var firstName = document.getElementByClassName("a1").namedItem("fname").value;
+    var lastName = document.getElementById("form1").elements.namedItem("lname").value;
+
+    // Check if both fields are not empty
+    if (firstName.trim() !== "" && lastName.trim() !== "") {
+        // Display an alert with the user's first and last name
+        alert(firstName +" "+ lastName);
+    } else {
+        
+        alert("Please fill in both First Name and Last Name fields.");
+    }
+
+    
+    return false;
+	
+	
+	
+
+	
+
 }
