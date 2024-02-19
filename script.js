e16a5979dbcee69967997476606e4fe5bd400c6f
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2024-02-19 11:35:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,19 +3,23 @@ function getFormvalue(ev) {
   var Name = document.querySelectorAll("#form1>input");
   var firstName = Name[0];
   var lastName = Name[1];
-  console.log(Name[0].value);
+  
 
 
-  if (firstName.value == "" || lastName.value == "") {
+  // if (firstName.value == "" || lastName.value == "") {
     
-    alert("Please fill in both First Name and Last Name fields.");
+  //   alert("Please fill in both First Name and Last Name fields.");
 
-  } else {
-    alert(firstName.value + " " + lastName.value);
+  // } else {
+  //   alert(firstName.value + " " + lastName.value);
 
 
-  }
-  ev.preventDefault();
+  // }
+  // ev.preventDefault();
+	var fullName = firstName.value + " " + lastName.value;
+
+    
+    alert(fullName);
 
 
 }
